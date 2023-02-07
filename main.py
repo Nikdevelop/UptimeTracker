e83097137896645716ce180a7497b266d01e4fb5
Никита <nizhizhin@gmail.com>
2023-02-07 21:42:50 +0300

    Modified base

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import flask
+from flask import render_template
+
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', debug=True)
